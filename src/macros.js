d85a4dbbb60e7fce606de84cabf76b67e68e51d4
Jason <jason.mobley5@gmail.com>
2023-03-07 06:06:39 -0500

    temporary code to show personalModal

--- src/macros.js
+++ src/macros.js
@@ -1,6 +1,10 @@
 const openMenu = document.getElementById("openMenu");
 const dropDown = document.getElementById("dropDown");
 
+const personalModal = document.getElementById("personalModal")
+
 openMenu.addEventListener('click', () => {
     dropDown.classList.toggle("dropdown-toggle")
 })
+
+personalModal.showModal()
