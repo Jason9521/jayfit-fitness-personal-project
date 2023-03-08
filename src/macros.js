4226e3358ac20694eae6493ca3ac835e21904c1a
Jason <jason.mobley5@gmail.com>
2023-03-08 06:09:45 -0500

    added event listeners for modals

--- src/macros.js
+++ src/macros.js
@@ -2,9 +2,21 @@ const openMenu = document.getElementById("openMenu");
 const dropDown = document.getElementById("dropDown");
 
 const personalModal = document.getElementById("personalModal")
+const personalClose = document.getElementById('personalClose')
+const macroModal = document.getElementById('macroModal')
+const macroClose = document.getElementById('macroClose')
 
 openMenu.addEventListener('click', () => {
     dropDown.classList.toggle("dropdown-toggle")
 })
 
-personalModal.showModal()
+personalClose.addEventListener('click', () => {
+    personalModal.close()
+})
+
+macroClose.addEventListener('click', () => {
+    macroModal.close
+})
+
+// personalModal.showModal()
+macroModal.showModal()
